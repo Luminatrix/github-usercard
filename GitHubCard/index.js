001e4502c8bdf4c4753ae7dc9cb4319122ff70bc
Stella <stella.kate.edelmann@gmail.com>
2019-10-31 19:47:17 -0700

    saving work

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,14 +1,75 @@
+// const entryPoint =
+// document.querySelector(".cards");
+
+
 /* Step 1: using axios, send a GET request to the following URL 
-           (replacing the palceholder with your Github name):
-           https://api.github.com/users/<your name>
-*/
+           (replacing the palceholder with your Github name):*/
+          //  https://api.github.com/users/<your name>
+
 
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
-   data in order to use it to build your component function 
+   data in order to use it to build your component function */
+//  Step 3: Create a function that accepts a single object as its only argument,
+//           Using DOM methods and properties, create a component that will return the following DOM element:
 
-   Skip to Step 3.
-*/
+function gitCard(data){
+  const newCard = document.createElement("div"),
+        newImage = document.createElement("img"),
+        newInfo = document.createElement("div"),
+        newName = document.createElement("h3"),
+        newUsername = document.createElement("p"),
+        newLocation = document.createElement("p"),
+        newProfile = document.createElement("a"),
+        newFollowers = document.createElement("p"),
+        newFollowing = document.createElement("p"),
+        newBio = document.createElement("p");
+
+    newImage.src = data.url;
+    newName.textContent = data.name;
+    newUsername.textContent = data.login;
+    newLocation.textContent = data.location;
+    newProfile.src = data.html_url;
+    newFollowers.textContent = data.followers;
+    newFollowing.textContent = data.following;
+    newBio.textContent = data.bio;
+
+
+    newCard.classList.add('card');
+    newInfo.classList.add('card-info');
+    newName.classList.add('name');
+    newUsername.classList.add('username');
+
+    
+    newCard.appendChild(newImage);
+    newCard.appendChild(newInfo);
+    newInfo.appendChild(newName);
+    newInfo.appendChild(newUsername);
+    newInfo.appendChild(newLocation);
+    newInfo.appendChild(newProfile);
+    newInfo.appendChild(newFollowers);
+    newInfo.appendChild(newFollowing);
+    newInfo.appendChild(newBio);
+
+//add eventlisteners here
+
+
+
+}
+
+axios.get("https://api.github.com/users/luminatrix")
+.then(response =>{
+  console.log(response);
+  response.forEach(data => {
+    const cards = document.querySelector('.cards');
+
+cards.appendChild(
+  gitCard(response.data)
+);
+  });
+});
+
+
 
 /* Step 4: Pass the data received from Github into your function, 
            create a new component and add it to the DOM as a child of .cards
@@ -24,27 +85,31 @@
           user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+const followersArray = ['cladams0203', 'fishmanjohn', 'coltynw', 'Rae-Glazier', 'weng7533', 'dijahdeen'];
 
-/* Step 3: Create a function that accepts a single object as its only argument,
-          Using DOM methods and properties, create a component that will return the following DOM element:
+followersArray.forEach((name) => {
 
-<div class="card">
-  <img src={image url of user} />
-  <div class="card-info">
-    <h3 class="name">{users name}</h3>
-    <p class="username">{users user name}</p>
-    <p>Location: {users location}</p>
-    <p>Profile:  
-      <a href={address to users github page}>{address to users github page}</a>
-    </p>
-    <p>Followers: {users followers count}</p>
-    <p>Following: {users following count}</p>
-    <p>Bio: {users bio}</p>
-  </div>
-</div>
+  axios.get(`https://api.github.com/users/${name}`)
 
-*/
+  //if successfull, do this!
+.then(response => {
+
+const cards = document.querySelector('.cards');
+
+cards.append(
+  gitCard(response.data)
+);
+console.log(response);
+})
+//if not successful do this!
+.catch(error => console.log(error));
+});
+
+
+
+
+
+
 
 /* List of LS Instructors Github username's: 
   tetondan
@@ -52,4 +117,11 @@ const followersArray = [];
   justsml
   luishrd
   bigknell
-*/
+
+/* Step 1: using axios, send a GET request to the following URL 
+           (replacing the palceholder with your Github name):*/
+          //  https://api.github.com/users/<your name>
+
+/* Step 2: Inspect and study the data coming back, this is YOUR 
+   github info! You will need to understand the structure of this 
+   data in order to use it to build your component function */
